d2ccfc121abdf92022dfac22e19c9b08250bebeb
Ziyi Liew <ziyiapril@gmail.com>
2020-12-16 01:14:10 +0800

    setup Spool, Objects, Printing VCs. Added elements to Add Spool view controller

---
+++ Matter/ViewControllers/ObjectVC.swift
@@ -0,0 +1,18 @@
+//
+//  ObjectVC.swift
+//  Matter
+//
+//  Created by Ziyi Liew on 16/12/20.
+//
+
+import UIKit
+
+class ObjectVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
