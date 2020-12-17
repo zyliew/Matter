bc845833588e0f544926023fd4332c568362c19b
Ziyi Liew <ziyiapril@gmail.com>
2020-12-17 19:58:53 +0800

    added alert to delete multiple spools in SpoolVC

--- Matter/ViewControllers/SpoolVC.swift
+++ Matter/ViewControllers/SpoolVC.swift
@@ -140,6 +140,39 @@ extension SpoolVC {
         print("deleted single data")
     }
     
+    func deleteMultipleData(uids: [String]) {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Spool")
+        
+        var fetchedResults: [NSManagedObject]
+        
+        do {
+            try fetchedResults = context.fetch(request) as! [NSManagedObject]
+            
+            if fetchedResults.count > 0 {
+                
+                for result:AnyObject in fetchedResults {
+                    let currentUID = result.value(forKey: "uid") as! String
+                    if uids.contains(currentUID) {
+                        context.delete(result as! NSManagedObject)
+                        print("Spool with UID: \(currentUID) deleted from Core Data")
+                    }
+                }
+            }
+            try context.save()
+            
+        } catch {
+            // if an error occurs
+            let nserror = error as NSError
+            NSLog("Unresolved error \(nserror), \(nserror.userInfo)")
+            abort()
+        }
+        
+        print("deleted multiple data")
+    }
+    
     // checks if spool is already in the spoolArray, add or increment count accordingly
     func checkAndAdd(compare: NSManagedObject) {
         print("check and add")
@@ -228,6 +261,16 @@ extension SpoolVC: UITableViewDelegate, UITableViewDataSource {
                 tableView.deleteRows(at: [indexPath], with: .fade)
             } else {
                 print("count more than 1. Can't delete")
+                let alert = UIAlertController(title: "Are you sure?", message: "\(spool.count) spools will be deleted", preferredStyle: .alert)
+
+                alert.addAction(UIAlertAction(title: "Yes", style: .default, handler: {action in
+                    self.deleteMultipleData(uids: spool.uids)
+                    spoolArray.remove(at: indexPath.row)
+                    tableView.deleteRows(at: [indexPath], with: .fade)
+                }))
+                alert.addAction(UIAlertAction(title: "No", style: .cancel, handler: nil))
+
+                self.present(alert, animated: true)
             }
             
         } else if editingStyle == .insert {
